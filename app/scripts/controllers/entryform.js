5386090f7295ca2ace19ce82d364a53c6e0a7b38
RBeek <redres@msn.com>
2014-05-28 19:53:10 +0200

    added date function for entryform

--- app/scripts/controllers/entryform.js
+++ app/scripts/controllers/entryform.js
@@ -1,16 +1,26 @@
 'use strict';
 
 angular.module('nokNokApp')
-  .controller('EntryformCtrl', function($scope, $location, $filter, SeCounters, guestbook) {
+  .controller('EntryformCtrl', function($scope, $location, $filter, $interval, SeCounters, guestbook) {
     $scope.guestbookcounter = SeCounters.get('guestbook');
     $scope.guestbook = guestbook;
 
-    var d = $filter('date')(new Date(), 'MM/dd/yyyy @ HH:mm')
-    $scope.guestbook.datetime = d;
+    $scope.$on('$destroy', function() {
+      // Make sure that the interval is destroyed too
+      $interval.cancel(addtime);
+    });
+
+    var addtime = $interval(function() {
+      var d = $filter('date')(new Date(), 'MM/dd/yyyy @ HH:mm');
+      $scope.guestbook.datetime = d;
+
+    }, 1000);
+
 
     var changeSuccess = function() {
       SeCounters.increase('guestbook');
       $scope.guestbookcounter = SeCounters.get('guestbook');
+      $interval.cancel(addtime);
       $location.path('/');
     };
 
